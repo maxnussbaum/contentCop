e9281e46cfe24f1f30d21bb89ac1fb15f65768a9
Max Nussbaum <maxnussbaum@live.com>
2019-02-09 17:41:05 -0500

    refactor(Lyric Retrieval per track): Clean up lyric retrieval for individual tracks

--- lib/contentCop/track.rb
+++ lib/contentCop/track.rb
@@ -1,6 +1,8 @@
 
+require_relative "./lyric_methods"
+
 class Track
-  attr_accessor :explicit, :id, :name, :uri, :album, :artists
+  attr_accessor :explicit, :id, :name, :uri, :album, :artists, :lyrics
   def initialize(args)
     @explicit = args["explicit"]
     @id = args["id"]
@@ -8,5 +10,10 @@ class Track
     @uri = args["uri"]
     @album = args["album"]
     @artists = args["artists"]
+  end
+
+  def find_lyrics
+    @lyrics ||= get_lyrics(@artists[0]["name"], @name)
+    puts @lyrics
   end
 end
