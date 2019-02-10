b443dc854c909ad8ba75a6e58df988db2ea8f4f8
Max Nussbaum <maxnussbaum@live.com>
2019-02-10 12:22:48 -0500

    feat:

--- visuals/myapp.rb
+++ visuals/myapp.rb
@@ -3,25 +3,26 @@ require "sinatra/reloader"
 require_relative("./../lib/contentCop")
 require "dotenv/load"
 require "http"
-require("httplog")
-HttpLog.configure do |config|
-  config.log_connect = true
-  config.log_request = true
-  config.log_headers = true
-  config.log_data = true
-  config.log_status = true
-  config.log_response = true
-end
+# require("httplog")
+# HttpLog.configure do |config|
+#   config.log_connect = true
+#   config.log_request = true
+#   config.log_headers = true
+#   config.log_data = true
+#   config.log_status = true
+#   config.log_response = true
+# end
 
 $a = ContentCop::User.new
 $a.username = ENV["USERNAME"]
 
 get "/" do
-  response = HTTP.follow.get("https://accounts.spotify.com/authorize?client_id=#{ENV["CLIENT_ID"]}&response_type=code&redirect_uri=localhost:4567/callback&scope=playlist-modify-public&show_dialog=true")
-  puts response.to_s
-  @body = response.to_s
-  erb :landing_page
-  #$a.client_init
+  # response = HTTP.follow.get("https://accounts.spotify.com/authorize?client_id=#{ENV["CLIENT_ID"]}&response_type=code&redirect_uri=http://localhost:4567/callback&scope=playlist-modify-public&show_dialog=true")
+  # puts response.to_s
+  # @body = response.to_s
+  # erb :landing_page
+  $a.client_init
+  redirect "/playlists"
 end
 
 get "/playlists" do
@@ -39,6 +40,11 @@ get "/playlist/:id" do
   erb :playlist_view
 end
 
-get "/callback" do
-  "CALLBACK"
-end
+# get "/callback" do
+#   "CALLBACK"
+# end
+
+# post "/api/login" do
+#   #puts "#{request.env}"
+#   #redirect "http://accounts.spotify.com/api/login"
+# end
